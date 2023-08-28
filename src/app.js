0963963b76e9e5fabac8bbed4b2fa7f07875c93b
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2023-08-28 12:53:31 +0200

    Added current location date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
+let now = new Date();
+let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[now.getMonth()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${hours}`;
+}
+
+let currentLocationDate = document.querySelector("#date");
+currentLocationDate.innerHTML = `${day} ${date}th ${month} ${hour}:${minutes}`;
+
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
